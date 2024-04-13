67d6579be6a183724fb4d4a41ebdbe3733f6cca2
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-04-13 00:50:08 -0400

    Update page title in index.html and App.jsx

--- crudgame/src/App.jsx
+++ crudgame/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <p>Test</p>
+      <p>Project 7 coming soon</p>
     </>
   )
 }
